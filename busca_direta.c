e508d80d0ff004b9e09cfc7f23c6e0a15567054a
Enzo Quartieri Adamo <75939399+EnzoQAdamo@users.noreply.github.com>
2024-07-03 14:02:11 -0300

    Correção da variavel com um 9 a mais

--- busca_direta.c
+++ busca_direta.c
@@ -21,6 +21,7 @@ int buscaDiretaNoArquivo(const char *nomeArquivo, const char *chave) {
         sscanf(linha, "%5s,%19[^,],%19[^,],%19[^\n]", registro.chave, registro.atributo2, registro.atributo3, registro.atributo4);
 
         if (strcmp(registro.chave, chave) == 0) {
+
             fclose(arquivo);
             return 1;  // Chave encontrada
         }
@@ -31,33 +32,24 @@ int buscaDiretaNoArquivo(const char *nomeArquivo, const char *chave) {
 }
 
 void buscaNaLinha(const char *nomeArquivo, int linha_desejada){
-    FILE *arquivo = fopen(nomeArquivo, "r");
-    if (arquivo == NULL) {
-        printf("Erro ao abrir o arquivo.\n");
+    FILE *file = fopen(nomeArquivo, "r");
+    if (!file) {
+        fprintf(stderr, "Erro ao abrir o arquivo.\n");
         return;
     }
 
-    // Tamanho de cada linha, incluindo o caractere de nova linha '\n'
-    int tamanho_linha = 48;  // Altere para o tamanho correto
+    char buffer[1024];
+    int current_line = 1;
 
-    // Calcular o deslocamento
-    long offset = (linha_desejada - 1) * tamanho_linha;
-
-    // Mover o ponteiro do arquivo para a linha desejada
-    if (fseek(arquivo, offset, SEEK_SET) != 0) {
-        perror("Erro ao mover o ponteiro do arquivo\n");
-        fclose(arquivo);
-        return;
+    while (fgets(buffer, sizeof(buffer), file)) {
+        if (current_line == linha_desejada) {
+            printf("Linha %d: %s", linha_desejada, buffer);
+            fclose(file);
+            return;
+        }
+        current_line++;
     }
 
-    // Ler a linha desejada
-    char buffer[tamanho_linha + 1]; // +1 para o caractere nulo
-    if (fgets(buffer, sizeof(buffer), arquivo) == NULL) {
-        perror("Erro ao ler a linha do arquivo\n");
-        fclose(arquivo);
-        return;
-    }
-
-    printf("Linha %d: %s", linha_desejada, buffer);
-    fclose(arquivo);
+    printf("O arquivo não possui a linha %d.\n", linha_desejada);
+    fclose(file);
 }
